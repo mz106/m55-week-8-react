b4e38cf3c650718170b17bc520ab78cae25d5366
Michael Zwierzanski <69387856+mz106@users.noreply.github.com>
2024-07-12 15:07:18 +0100

    feature: add message change on addAuthor

--- src/components/addAuthor/AddAuthor.jsx
+++ src/components/addAuthor/AddAuthor.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { changeHandler } from "../../utils/helpers";
 import { addAuthor } from "../../utils/authorFetch";
 
+import { MessageContext } from "../../providers/messageProvider/MessageProvider";
+
 const AddAuthor = () => {
   const [author, setAuthor] = useState("");
+
+  const { updateMessage } = useContext(MessageContext);
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -12,6 +16,13 @@ const AddAuthor = () => {
 
     const data = await addAuthor(author);
     console.log("data in addAuthor: ", data);
+
+    // data.author.authorName
+
+    const newMessage = `You have added the author ${data.author.authorName}`;
+
+    updateMessage(e, newMessage);
+    setAuthor("");
   };
 
   return (
